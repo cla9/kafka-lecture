e4f67ad2ab1c2b2501a544b723fe1cf6449b9c8d
eunsoo kim <cla9@nate.com>
2023-06-27 22:15:43 +0900

    add describe topic into 11, 12 scripts

--- self/12_reassign_partition.sh
+++ self/12_reassign_partition.sh
@@ -2,3 +2,6 @@
 
 export BASE=$(pwd)
 $HOME/kafka_2.13-3.5.0/bin/kafka-reassign-partitions.sh --bootstrap-server localhost:9092,localhost:9094,localhost:9096 --reassignment-json-file $BASE/json/12_reassign_partition_manual.json --execute
+
+echo "\n\n"
+./08_describe_topic.sh
