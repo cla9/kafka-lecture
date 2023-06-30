99236c9dcac8cb674ddf1208171aca1faa47502c
eunsoo kim <cla9@nate.com>
2023-06-30 14:17:26 +0900

    update shared offset

--- 01_shared/02_consume_beginning_offset.sh
+++ 01_shared/02_consume_beginning_offset.sh
@@ -2,4 +2,4 @@
 
 bash ../util/check_variable.sh
 
-$KAFKA_HOME/bin/kafka-console-consumer.sh --topic kafka-class-2 --bootstrap-server $SERVER_IP:9092 --from-beginning
+$KAFKA_HOME/bin/kafka-console-consumer.sh --topic kafka-consumer-test --bootstrap-server $SERVER_IP:9092 --from-beginning
