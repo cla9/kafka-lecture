95e546f15b9d609d1b9f8be399f2f79b9755ef9d
cla9 <cla9@nate.com>
2024-05-07 11:50:52 +0900

    update kafka version

--- 01_self/00_pre_setting/01_install_kafka.sh
+++ 01_self/00_pre_setting/01_install_kafka.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-curl -LO https://downloads.apache.org/kafka/3.5.0/kafka_2.13-3.5.0.tgz 
-tar -xvf kafka_2.13-3.5.0.tgz -C $HOME
+curl -LO https://downloads.apache.org/kafka/3.7.0/kafka_2.13-3.7.0.tgz 
+tar -xvf kafka_2.13-3.7.0.tgz -C $HOME
 
--- 01_self/00_pre_setting/02_set_environment_P.sh
+++ 01_self/00_pre_setting/02_set_environment_P.sh
@@ -4,7 +4,7 @@
 
 if ! grep -q "KAFKA_HOME" ~/.bashrc ; then
      # Export the value to `.bashrc`  
-     echo "export KAFKA_HOME=$HOME/kafka_2.13-3.5.0" >> ~/.bashrc
+     echo "export KAFKA_HOME=$HOME/kafka_2.13-3.7.0" >> ~/.bashrc
 fi
 # Source `.bashrc` to apply the changes
 source ~/.bashrc 
